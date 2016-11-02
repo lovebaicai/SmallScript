c94783531e65895f7877c57a31a0f537bc27e906
lovebaicai <cfqtxd1@hotmail.com>
2016-11-02 18:49:26 +0800

    fix meta bug

--- spider/xmlyspider/xmlyspider/spiders/ximalaya.py
+++ spider/xmlyspider/xmlyspider/spiders/ximalaya.py
@@ -6,48 +6,42 @@ import scrapy
 from datetime import datetime
 from ..items import XmlyspiderItem
 
-
 class XimalayaSpider(scrapy.Spider):
     name = 'xmly'
     allowed_domains = ['http://mobile.ximalaya.com/']
-    start_urls = (
-        'http://mobile.ximalaya.com/mobile/discovery/v2/category/metadata/albums?'
-        'calcDimension=hot&categoryId=33&device=android&pageId=1&pageSize=620&version=5.4.39',
-    )
+    start_urls = [
+        'http://mobile.ximalaya.com/mobile/discovery/v2/category/metadata/albums?calcDimension=hot&categoryId=33&device=android&pageId=1&pageSize=620&version=5.4.39'
+    ]
 
     def parse(self, response):
         pagecode = json.loads(response.body)['list']
-        #print pagecode
         item = XmlyspiderItem()
         for info in pagecode:
             item['Albumtitle'] = info['title']
+#            item['AlbumId'] = info['albumId']
             item['Albumscore'] = info['score']
             item['TotalPlayCounts'] = info['playsCounts']
             item['displayDiscountedPrice'] = info['displayDiscountedPrice']
             AlbumId = info['albumId']
-            albumurl = (
-               'http://mobile.ximalaya.com/mobile/v1/album/track?albumId=%s&device=android&pageId=1&pageSize=200'
-               ) % AlbumId
-            yield scrapy.Request(albumurl, meta={'item': item}, callback=self.parse_album, dont_filter=True)
-
-    def parse_album(self, response):
-        item = response.meta['item']
-        code = json.loads(response.body)['data']['list']
-        #print code
-        for info1 in code:
-            item['Title'] = info1['title']
-            item['Nickname'] = info1['nickname']
-            item['SinglePlayCount'] = info1['playtimes']
-            CreatedTime = int(info1['createdAt']) / 1000
-            item['CreatedTime'] = datetime.fromtimestamp(CreatedTime).strftime('%Y-%m-%d')
-            item['Duration'] = info1['duration']
-            item['LikeCount'] = info1['likes']
-            item['CommentsCount'] = info1['comments']
-            trackId = info1['trackId']
-            singleurl = 'http://www.ximalaya.com/tracks/%s.json' % trackId
-            item['category_title'] = json.loads(requests.get(singleurl).content)['category_title']
-            yield item
-xcept:
+            albumurl = ('http://mobile.ximalaya.com/mobile/v1/album/track?albumId='
+                        '%s&device=android&pageId=1&pageSize=200') % AlbumId
+            try:
+                code = json.loads(requests.get(albumurl, timeout=20).content)['data']['list']
+            #item = XmlyspiderItem()
+                for info in code:
+                    item['Title'] = info['title']
+                    item['Nickname'] = info['nickname']
+                    item['SinglePlayCount'] = info['playtimes']
+                    CreatedTime = int(info['createdAt']) / 1000
+                    item['CreatedTime'] = datetime.fromtimestamp(CreatedTime).strftime('%Y-%m-%d')
+                    item['Duration'] = info['duration']
+                    item['LikeCount'] = info['likes']
+                    item['CommentsCount'] = info['comments']
+                    trackId = info['trackId']
+                    singleurl = 'http://www.ximalaya.com/tracks/%s.json' % trackId
+                    item['category_title'] = json.loads(requests.get(singleurl).content)['category_title']
+                    yield item
+            except:
                 pass
 
 
