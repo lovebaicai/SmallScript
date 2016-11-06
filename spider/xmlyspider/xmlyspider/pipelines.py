a67d0de7479d4195902bc6b9be2325fefc41d90c
lovebaicai <cfqtxd1@hotmail.com>
2016-11-06 17:55:11 +0800

    add everyday update mysql

--- spider/xmlyspider/xmlyspider/pipelines.py
+++ spider/xmlyspider/xmlyspider/pipelines.py
@@ -6,8 +6,10 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from scrapy.conf import settings
+from scrapy.exceptions import DropItem
 #from scrapy import log
 import pymongo
+import datetime
 import MySQLdb
 import sys
 reload(sys)
@@ -28,28 +30,35 @@ sys.setdefaultencoding('utf8')
 #         self.collection.insert(dict(item))
 #         return item
 
-#Mysql
 class XmlyMysqlPipeline(object):
+
     def __init__(self):
-        self.conn = MySQLdb.connect(host="localhost", user="root", passwd="ubuntu", db="sound", charset="utf8")
+        time1 = datetime.datetime.now().strftime('%Y-%m-%d').replace('-','')
+        self._tabname = 'sound_' + time1
+        self.conn = MySQLdb.connect(host="localhost", user="root", passwd="ubuntu", db="spider", charset="utf8")
         self.cursor = self.conn.cursor()
         try:
-            self.cursor.execute(
-                "create table paidsound(id int PRIMARY KEY AUTO_INCREMENT,Albumtitle varchar (255), Albumscore varchar(255),"
-                "TotalPlayCounts VARCHAR(255), displayDiscountedPrice VARCHAR(255), Title VARCHAR (255), "
-                "Nickname VARCHAR (255), SinglePlayCount VARCHAR (255), CreatedTime VARCHAR (255), Duration VARCHAR (255),"
-                "LikeCount VARCHAR (255), CommentsCount VARCHAR (255), category_title VARCHAR (255))")
+            sql = """CREATE TABLE %s (
+                id int PRIMARY KEY AUTO_INCREMENT, Title varchar (255), Albumtitle varchar(255),
+                SinglePlayCount VARCHAR(255), Albumscore VARCHAR(255), LikeCount VARCHAR (255),
+                CommentsCount VARCHAR (255), TotalPlayCounts VARCHAR (255), Duration VARCHAR (255),
+                CreatedTime VARCHAR (255), displayDiscountedPrice VARCHAR (255),
+                Nickname VARCHAR (255), category_title VARCHAR (255), trackId VARCHAR (255)
+                 )""" % self._tabname
+            self.cursor.execute(sql)
         except:
             pass
 
-    def process_item(self, item, spider):
-        self.cursor.execute(
-            "insert into paidsound(Albumtitle, Albumscore, TotalPlayCounts, displayDiscountedPrice, Title, Nickname, " \
-            "SinglePlayCount, CreatedTime, Duration, LikeCount, CommentsCount, category_title) values " \
-            "('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
-             % (item['Albumtitle'], item['Albumscore'], item['TotalPlayCounts'], item['displayDiscountedPrice'],
-                  item['Title'], item['Nickname'], item['SinglePlayCount'], item['CreatedTime'],
-                  item['Duration'],item['LikeCount'], item['CommentsCount'], item['category_title']))
-        self.conn.commit()
 
+    def process_item(self, item, spider):
+        sql = ("insert into {} (Title, Albumtitle, SinglePlayCount, Albumscore, LikeCount, CommentsCount, \
+                TotalPlayCounts, Duration, CreatedTime, displayDiscountedPrice, Nickname, category_title, trackId) \
+                values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
+                (item['Title'], item['Albumtitle'], item['SinglePlayCount'], item['Albumscore'], \
+                item['LikeCount'], item['CommentsCount'], item['TotalPlayCounts'], item['Duration'], \
+                item['CreatedTime'], item['displayDiscountedPrice'], item['Nickname'], item['category_title'], \
+                item['trackId'])).format(self._tabname)
+
+        self.cursor.execute(sql)
+        self.conn.commit()
 
