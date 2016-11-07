e28bef5b3d2c9ad54c831558c781b05d58608b5b
lovebaicai <cfqtxd1@hotmail.com>
2016-11-07 10:43:52 +0800

    add trackId

--- spider/xmlyspider/xmlyspider/spiders/ximalaya.py
+++ spider/xmlyspider/xmlyspider/spiders/ximalaya.py
@@ -7,10 +7,12 @@ from datetime import datetime
 from ..items import XmlyspiderItem
 
 class XimalayaSpider(scrapy.Spider):
+
     name = 'xmly'
     allowed_domains = ['http://mobile.ximalaya.com/']
     start_urls = [
-        'http://mobile.ximalaya.com/mobile/discovery/v2/category/metadata/albums?calcDimension=hot&categoryId=33&device=android&pageId=1&pageSize=620&version=5.4.39'
+        'http://mobile.ximalaya.com/mobile/discovery/v2/category/metadata/albums?calcDimension=hot'
+        '&categoryId=33&device=android&pageId=1&pageSize=620&version=5.4.39'
     ]
 
     def parse(self, response):
@@ -18,7 +20,6 @@ class XimalayaSpider(scrapy.Spider):
         item = XmlyspiderItem()
         for info in pagecode:
             item['Albumtitle'] = info['title']
-#            item['AlbumId'] = info['albumId']
             item['Albumscore'] = info['score']
             item['TotalPlayCounts'] = info['playsCounts']
             item['displayDiscountedPrice'] = info['displayDiscountedPrice']
@@ -27,7 +28,6 @@ class XimalayaSpider(scrapy.Spider):
                         '%s&device=android&pageId=1&pageSize=200') % AlbumId
             try:
                 code = json.loads(requests.get(albumurl, timeout=20).content)['data']['list']
-            #item = XmlyspiderItem()
                 for info in code:
                     item['Title'] = info['title']
                     item['Nickname'] = info['nickname']
@@ -37,6 +37,7 @@ class XimalayaSpider(scrapy.Spider):
                     item['Duration'] = info['duration']
                     item['LikeCount'] = info['likes']
                     item['CommentsCount'] = info['comments']
+                    item['trackId'] = info['trackId']
                     trackId = info['trackId']
                     singleurl = 'http://www.ximalaya.com/tracks/%s.json' % trackId
                     item['category_title'] = json.loads(requests.get(singleurl).content)['category_title']
