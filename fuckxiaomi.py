0822dffe29709fd2c5782ba848dafe2661192b2f
chenyu <chenyu@gshopper.com>
2017-05-16 14:13:07 +0800

    update click code

--- fuckxiaomi.py
+++ fuckxiaomi.py
@@ -24,31 +24,25 @@ password = driver.find_element_by_id('pwd')
 submit = driver.find_element_by_id('login-button')
 account.clear()
 password.clear()
-account.send_keys('username')
-password.send_keys('password')
+account.send_keys('username') #input your mi login username
+password.send_keys('password') #input your mi login password
 submit.click()
 time.sleep(0.5)
 cookies = driver.get_cookies()
 
 # swich page of buy mi6
-a = int(datetime.datetime.now().strftime('%H%M%S'))
-b = int('100000')
-c = b - a
-while c >0:
-    c -= 1
-    print(('Remaining time: {}s').format(c))
-    time.sleep(1)
 
 print('start Snapped up m6, fuck xiaomi')
 
+p = 0
 while 1:
     try:
         driver.find_element_by_xpath('//*[@id="J_buyBtnBox"]/li/a').click()
+        p += 1
+        print('click %s times' % p)
     except:
         pass
-    if (driver.find_element_by_xpath('//*[@id="J_buyBtnBox"]/li/a').click()) == True:
-        break
 
-time.sleep(5)
+time.sleep(120)
 driver.close()
 
