267724984250b8e7b9d64cc887966ae9ce67ec74
chenyu <chenyu@gshopper.com>
2017-06-28 19:39:12 +0800

    update redis use set

--- re_insert.sh
+++ re_insert.sh
@@ -3,7 +3,7 @@
 FILE=url.txt
 
 cat $FILE | while read line; do
-echo $line | tr -d '\n' | redis-cli -x set "url" >> redis.log
+echo $line | tr -d '\n' | redis-cli -x SADD "url" >> redis.log
 echo "Success insert line: $line"
 done
 echo "Total number of lines in file: $i"
