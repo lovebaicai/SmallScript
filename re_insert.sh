a2d950de67b7d3fb9dfc38375478dd506e5f9027
chenyu <chenyu@gshopper.com>
2017-06-28 19:17:38 +0800

    add pipelining to redis

---
+++ re_insert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILE=url.txt
+
+cat $FILE | while read line; do
+echo $line | tr -d '\n' | redis-cli -x set "url" >> redis.log
+echo "Success insert line: $line"
+done
+echo "Total number of lines in file: $i"
