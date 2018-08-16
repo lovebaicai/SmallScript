7e19d3c227297e8ef841a961137e8a625a0780b3
messay <cfqtxd1@hotmail.com>
2018-08-16 20:14:18 +0800

    add thunder script

---
+++ thunder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+chmod a-x /Applications/Thunder.app/Contents/Bundles/XLPlayer.app # 迅雷播放器
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/bbassistant.xlplugin # 宽带助手
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/browserhelper.xlplugin # 浏览器支持
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/featuredpage.xlplugin # 精选
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/playgame.xlplugin # 玩一玩
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/subtitle.xlplugin # 搜索字幕
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/thunderstore.xlplugin # 软件中心
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/xiazaibao.xlplugin # 下载宝
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/xlbrowser.xlplugin # 搜索
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/xlplayer.xlplugin # 迅雷播放器
+chmod a+x /Applications/Thunder.app/Contents/PlugIns/details.xlplugin # 任务详情
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/advertising.xlplugin #左下角live提示广告
