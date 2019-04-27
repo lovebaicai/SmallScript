3022f79f35b3b928290e8993f83e2884a656dbef
nemo.chen <cfqtxd1@gmail.com>
2019-04-27 21:16:55 +0800

    迅雷3.3.2版本去除无用功能

--- thunder.sh
+++ thunder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod a-x /Applications/Thunder.app/Contents/Bundles/XLPlayer.app # 迅雷播放器
+chmod a-x /Applications/Thunder.app/Contents/Bundles/xlplayer.xlplugin # 迅雷播放器
 chmod a-x /Applications/Thunder.app/Contents/PlugIns/bbassistant.xlplugin # 宽带助手
 chmod a-x /Applications/Thunder.app/Contents/PlugIns/browserhelper.xlplugin # 浏览器支持
 chmod a-x /Applications/Thunder.app/Contents/PlugIns/featuredpage.xlplugin # 精选
@@ -10,4 +10,7 @@ chmod a-x /Applications/Thunder.app/Contents/PlugIns/xiazaibao.xlplugin # 下载
 chmod a-x /Applications/Thunder.app/Contents/PlugIns/xlbrowser.xlplugin # 搜索
 chmod a-x /Applications/Thunder.app/Contents/PlugIns/xlplayer.xlplugin # 迅雷播放器
 chmod a+x /Applications/Thunder.app/Contents/PlugIns/details.xlplugin # 任务详情
-chmod a-x /Applications/Thunder.app/Contents/PlugIns/advertising.xlplugin #左下角live提示广告
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/advertising.xlplugin # 左下角live提示广告
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/softmanager.xlplugin # 软件管理
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/iOSThunder.xlplugin  # 手机迅雷
+chmod a-x /Applications/Thunder.app/Contents/PlugIns/activitycenter.xlplugin  # 活动中心
