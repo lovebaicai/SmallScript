32502e3b06489a64904d765a5b8a95f1d99659b5
nemo.chen <cfqtxd1@gmail.com>
2020-06-02 09:29:37 +0800

    update

--- ss_install.sh
+++ ss_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo yum install -y epel-release
-sudo yum install -y vim git wget zsh telnet docker docker-compose 
-sudo systemctl enable docker
-sudo systemctl start docker
 
-sudo docker run -d --restart=always -e PASSWORD=qf!!@6I9ZeEuyOgC -p8388:8388 -p8388:8388/udp -d shadowsocks/shadowsocks-libev
+yum install -y epel-release
+yum install -y vim git wget zsh telnet docker docker-compose 
+systemctl enable docker
+systemctl start docker
+
+docker run -d --restart=always -e PASSWORD=qf!!@6I9ZeEuyOgC -p8388:8388 -p8388:8388/udp -d shadowsocks/shadowsocks-libev
